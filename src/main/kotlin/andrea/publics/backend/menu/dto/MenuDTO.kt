9ae5ae236909f7ff9dbe79ad60a963c48c624bdb
sanghoonseo <summit1991@naver.com>
2022-09-19 19:59:09 +0900

    메뉴 id 문자열로 변경

--- src/main/kotlin/andrea/publics/backend/menu/dto/MenuDTO.kt
+++ src/main/kotlin/andrea/publics/backend/menu/dto/MenuDTO.kt
@@ -1,7 +1,7 @@
 package andrea.publics.backend.menu.dto
 
 data class MenuGroupDTO(
-    val id: Int,
+    val id: String,
     val title: String,
     val items: List<MenuItemDTO> = emptyList()
 ) {
@@ -9,44 +9,44 @@ data class MenuGroupDTO(
         val dummyMenu: List<MenuGroupDTO>
             get() = listOf(
                 MenuGroupDTO(
-                    id = 1,
-                    title = "일번그룹",
+                    id = "1",
+                    title = "Theme",
                     items = listOf(
                         MenuItemDTO(
-                            id = 2,
-                            name = "일번메뉴",
-                            path = "/first/one"
+                            id = "2",
+                            name = "Colors",
+                            path = "/template/theme/colors"
                         ),
                         MenuItemDTO(
-                            id = 3,
-                            name = "이번메뉴",
-                            path = "/first/two"
+                            id = "3",
+                            name = "Typography",
+                            path = "/template/theme/typography"
                         )
                     )
                 ),
                 MenuGroupDTO(
-                    id = 4,
-                    title = "일번그룹",
+                    id = "4",
+                    title = "COMPONENTS",
                     items = listOf(
                         MenuItemDTO(
-                            id = 5,
-                            name = "일번메뉴",
-                            path = "/second/first"
+                            id = "5",
+                            name = "Base",
+                            path = "/template/component/base"
                         ),
                         MenuItemDTO(
-                            id = 6,
-                            name = "이번메뉴",
-                            path = "/second/two",
+                            id = "6",
+                            name = "Buttons",
+                            path = "/template/component/buttons",
                             children = listOf(
                                 MenuItemDTO(
-                                    id = 7,
-                                    name = "일번메뉴",
-                                    path = "/second/two/one"
+                                    id = "7",
+                                    name = "Accordion",
+                                    path = "/template/component/buttons/accordion"
                                 ),
                                 MenuItemDTO(
-                                    id = 8,
-                                    name = "일번메뉴",
-                                    path = "/second/two/two"
+                                    id = "8",
+                                    name = "Breadcrumb",
+                                    path = "/template/component/buttons/breadcrumb"
                                 ),
                             )
                         )
@@ -57,8 +57,9 @@ data class MenuGroupDTO(
 }
 
 data class MenuItemDTO(
-    val id: Long,
+    val id: String,
     val name: String,
     val path: String,
     val children: List<MenuItemDTO> = emptyList()
 )
+
